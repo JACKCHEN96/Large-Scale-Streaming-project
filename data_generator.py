cd11d073a26c203674152fdd133afe51a32da8a1
JACKCHEN96 <cchenwenjie0901@gmail.com>
2020-04-07 01:19:54 -0400

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -13,56 +13,56 @@ f1=faker.Factory
 
 # for i in range (100):
 
+class data_generator:
+    """
+    A complete CRD
+    """
+    def __init__(self,ID=None):
+        # self.ID=ID
+        pass
 
-def gen_ID():
-    return uuid.uuid1()
+    def gen_ID(self):
+        return uuid.uuid1()
 
-def gen_callnumber():
-    callnumber=f1.create()
-    return callnumber.phone_number()
+    def gen_callnumber(self):
+        callnumber=f1.create()
+        return callnumber.phone_number()
 
-def gen_callednumber():
-    callednumber=f1.create()
-    return callednumber.phone_number()
+    def gen_callednumber(self):
+        callednumber=f1.create()
+        return callednumber.phone_number()
 
-def gen_teltime():
-    time=f1.create()
-    starttime=time.iso8601(tzinfo=None)
-    timedelta=random.randint(0,60*1000)
-    return starttime+"|"+str(timedelta)
+    def gen_teltime(self):
+        time=f1.create()
+        starttime=time.iso8601(tzinfo=None)
+        timedelta=random.randint(0,60*1000)
+        return starttime+"|"+str(timedelta)
 
-def gen_teltype():
-    if(random.randint(0,1)==0):
-        return "SMS"
-    else:
-        return "VOICE"
+    def gen_teltype(self):
+        if(random.randint(0,1)==0):
+            return "SMS"
+        else:
+            return "VOICE"
 
-def gen_charge():
-    return random.random()
+    def gen_charge(self):
+        return random.random()
 
-def gen_result():
-    r=random.randint(0,2)
-    if(r==0):
-        return "AWNSERED"
-    elif (r==1):
-        return "BUSY"
-    else:
-        return "VOICE"
+    def gen_result(self):
+        r=random.randint(0,2)
+        if(r==0):
+            return "AWNSERED"
+        elif (r==1):
+            return "BUSY"
+        else:
+            return "VOICE"
 
-# def gen_data(self):
-#     return str(self.en_ID())+"|"+str(self.gen_callnumber())\
-#            +str(self.gen_callednumber())+"|"+str(self.gen_teltime())\
-#            +str(self.gen_teltype())+"|"+str(self.gen_charge())+"|"+str(self.gen_result())
-def gen_data():
-    return str(gen_ID())+"|"+str(gen_callnumber())+"|"\
-           +str(gen_callednumber())+"|"+str(gen_teltime())+"|"\
-           +str(gen_teltype())+"|"+str(gen_charge())+"|"+str(gen_result())
+    def gen_data(self):
+        # self.ID=self.gen_ID()
+        return str(self.gen_ID())+"|"+str(self.gen_callnumber())+"|"\
+               +str(self.gen_callednumber())+"|"+str(self.gen_teltime())+"|"\
+               +str(self.gen_teltype())+"|"+str(self.gen_charge())+"|"+str(self.gen_result())
 
-# print(gen_ID())
-# print(gen_callnumber())
-# print(gen_callednumber())
-# print(gen_teltime())
-# print(gen_teltype())
-# print(gen_charge())
-# print(gen_result())
-print(gen_data())
+d1=data_generator()
+d2=data_generator()
+print(d1.gen_data())
+print(d2.gen_data())
