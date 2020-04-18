0e552bb3e873b6a962ab14d285755b733da355ea
JACKCHEN96 <cchenwenjie0901@gmail.com>
2020-04-17 22:49:30 -0400

    抱歉写的贼丑
    加了tutorial url方便看懂

--- data_generator.py
+++ data_generator.py
@@ -7,6 +7,7 @@ import random
 import datetime
 import uuid
 import redis
+import numpy as np
 
 f1=faker.Factory
 rds = redis.Redis(host='localhost', port=6379, decode_responses=True, db=0)   # host是redis主机，需要redis服务端和客户端都启动 redis默认端口是6379
@@ -16,10 +17,10 @@ class data_generator:
     """
     A complete CRD
     """
-    def __init__(self,ID=None,callednumber=None,teltime=None,teltype=None,charge=None,result=None,type=None):
+    def __init__(self,ID=None,callednumber=None,teltime=None,teltype=None,charge=None,result=None,type=None,pick_call_distribution="default", delta_distribution="default"):
         self.ID=self.gen_ID()
         self.callednumber=self.gen_callednumber()
-        self.teltime=self.gen_teltime()
+        self.teltime=self.gen_teltime(pick_call_distribution,delta_distribution)
         self.teltype=self.gen_teltype()
         self.charge=self.gen_charge()
         self.result=self.gen_result()
@@ -31,7 +32,7 @@ class data_generator:
         if charge is not None: self.charge = charge
         if result is not None: self.result = result
         if type is not None: self.type=type
-        self.output_redis_2()
+        # self.output_redis_2()
 
     def __str__(self):
         data = str(self.ID)+"|"\
@@ -46,10 +47,66 @@ class data_generator:
         callednumber=f1.create()
         return callednumber.phone_number()
 
-    def gen_teltime(self):
+    def gen_teltime(self,pick_call_distribution,delta_distribution):
+        """
+        A dummy way to do this, to be continued.
+        """
+        r=random.randint(0,6)
+        i=1
+        if(r==0):
+            call_distribution="midnight mode"
+        elif (r==1):
+            call_distribution="morning mode"
+        elif (r==2):
+            call_distribution="afternoon mode"
+        elif (r==3):
+            call_distribution="evening mode"
+        else:
+            call_distribution=pick_call_distribution
+
         time=f1.create()
-        starttime=time.iso8601(tzinfo=None)
-        timedelta=random.randint(0,60*1000)
+        if(call_distribution=="midnight mode"):
+            # TODO
+            starttime = time.iso8601(tzinfo=None)
+            while(starttime.split("T")[1].split(":")[0]>"6" and i<4):
+                starttime = time.iso8601(tzinfo=None)
+                i+=1
+        elif(call_distribution=="morning mode"):
+            # TODO
+            starttime = time.iso8601(tzinfo=None)
+            while(starttime.split("T")[1].split(":")[0]>"12" or starttime.split("T")[1].split(":")[0]<"6" and i<4):
+                starttime = time.iso8601(tzinfo=None)
+                i+=1
+        elif(call_distribution=="afternoon mode"):
+            starttime = time.iso8601(tzinfo=None)
+            while(starttime.split("T")[1].split(":")[0]>"18" or starttime.split("T")[1].split(":")[0]<"12" and i<4):
+                starttime = time.iso8601(tzinfo=None)
+                i+=1
+        elif(call_distribution=="evening mode"):
+            starttime = time.iso8601(tzinfo=None)
+            while(starttime.split("T")[1].split(":")[0]<"18" and i<4):
+                starttime = time.iso8601(tzinfo=None)
+                i+=1
+        else:
+            starttime = time.iso8601(tzinfo=None)
+
+        # If you are confused of distribution command, please refer to this tutorial:
+        # https://blog.csdn.net/howhigh/article/details/78007317
+        if(delta_distribution=="exponential"):
+            # TODO
+            timedelta = np.random.exponential(600, 1)[0]
+        elif(delta_distribution=="poisson"):
+            # TODO
+            timedelta = np.random.poisson(600, 1)[0]
+        elif(delta_distribution=="binomial"):
+            # TODO
+            timedelta = np.random.binomial(1200,0.5,1)[0]
+        else:
+            timedelta = random.randint(0, 6000)
+            # timedelta = np.random.exponential(600, 1)[0]
+            # timedelta = np.random.poisson(600, 1)[0]
+            # timedelta = np.random.binomial(1200, 0.5, 1)[0]
+
         return starttime+"|"+str(timedelta)
 
     def gen_teltype(self):
@@ -106,7 +163,7 @@ class people:
         for i in range(self.calltimes):
             tempdata=str(self.ID)+"|"+str(self.callnumber)+"|"\
                      +(str(self.data[i]))
-            self.output_redis_1(self.data[i].ID,tempdata)
+            # self.output_redis_1(self.data[i].ID,tempdata)
             fuldata+=tempdata+"\n"
         return fuldata
 
@@ -142,14 +199,14 @@ p1=people()
 # test people
 print("##############test people###########################")
 print(p1)
-# test output_redis
-print("##############test output_redis#######################")
-print(rds[str(p1.data[0].ID)])
-# test iterate redis data
-print("##############test iterate redis data#################")
-keys=rds.keys()
-print(keys)
-# test iterate redis_type data
-print("##############test iterate redis_type data############")
-keys_type=rds_type.keys()
-print(keys_type)
+# # test output_redis
+# print("##############test output_redis#######################")
+# print(rds[str(p1.data[0].ID)])
+# # test iterate redis data
+# print("##############test iterate redis data#################")
+# keys=rds.keys()
+# print(keys)
+# # test iterate redis_type data
+# print("##############test iterate redis_type data############")
+# keys_type=rds_type.keys()
+# print(keys_type)
