3a643c95eb26218f038cb47de93e2a79edc10d33
huchong <1178869226@qq.com>
2020-04-27 04:51:18 -0400

    [ADD] prepare for merge

--- cdr_controller/data_generator.py
+++ cdr_controller/data_generator.py
@@ -1,134 +1,155 @@
 __author__ = "Wenjie Chen"
 __email__ = "wc2685@columbia.edu"
 
-# from faker import Faker
-import faker
 import random
-import datetime
 import uuid
-import redis
+from random import uniform
+from time import *
+
 import numpy as np
+import redis
 from phonenumbers.phonenumberutil import region_code_for_country_code
 
-f1=faker.Factory
-rds = redis.Redis(host='localhost', port=6379, decode_responses=True, db=0)   # host是redis主机，需要redis服务端和客户端都启动 redis默认端口是6379
+# f1=faker.Factory
+rds = redis.Redis(host='localhost', port=6379, decode_responses=True,
+                  db=0)  # host是redis主机，需要redis服务端和客户端都启动 redis默认端口是6379
 rds_type = redis.Redis(host='localhost', port=6379, decode_responses=True, db=1)
-rds_type_2 =  redis.Redis(host='localhost', port=6379, decode_responses=True, db=2)
+rds_type_2 = redis.Redis(host='localhost', port=6379, decode_responses=True,
+                         db=2)
 
 
 class data_generator:
     """
     A complete CRD
     """
-    def __init__(self,ID=None,callednumber=None,teltime=None,teltype=None,charge=None,result=None,type=None,
+
+    def __init__(self, ID=None, callednumber=None, teltime=None, teltype=None,
+                 charge=None, result=None, type=None,
                  pick_type_distribution="default", rate_type_distribution=0.1,
                  pick_call_distribution="default", delta_distribution="default",
                  rate_place_distribution=0.7):
 
-        self.ID=self.gen_ID()
-        self.callednumber=self.gen_callednumber(rate_place_distribution)
-        self.teltime=self.gen_teltime(pick_call_distribution,delta_distribution)
-        self.teltype=self.gen_teltype()
-        self.charge=self.gen_charge()
-        self.result=self.gen_result()
-        self.type=self.gen_type(pick_type_distribution, rate_type_distribution)
+        self.ID = self.gen_ID()
+        self.callednumber = self.gen_callednumber(rate_place_distribution)
+        self.teltime = self.gen_teltime(pick_call_distribution,
+                                        delta_distribution)
+        self.teltype = self.gen_teltype()
+        self.charge = self.gen_charge()
+        self.result = self.gen_result()
+        self.type = self.gen_type(pick_type_distribution,
+                                  rate_type_distribution)
 
-        if ID is not None: self.ID=ID
+        if ID is not None: self.ID = ID
         if callednumber is not None: self.callednumber = callednumber
         if teltime is not None: self.telltime = teltime
         if teltype is not None: self.teltype = teltype
         if charge is not None: self.charge = charge
         if result is not None: self.result = result
-        if type is not None: self.type=type
+        if type is not None: self.type = type
         self.output_redis_2()
 
     def __str__(self):
-        data = str(self.ID)+"|"\
-               +str(self.callednumber)+"|"+str(self.teltime)+"|"\
-               +str(self.teltype)+"|"+str(self.charge)+"|"+str(self.result)
+        data = str(self.ID) + "|" \
+               + str(self.callednumber) + "|" + str(self.teltime) + "|" \
+               + str(self.teltype) + "|" + str(self.charge) + "|" + str(
+            self.result)
         return data
 
     def gen_ID(self):
         return uuid.uuid1()
 
     def gen_callednumber(self, rate_place_distribution):
-        r = random.randint(1,100)
-        if r<100*rate_place_distribution:
-            place="1"
+        r1 = random.randint(1, 100)
+        if r1 < 100 * rate_place_distribution:
+            place = "1"
         else:
             place = random.randint(1, 300)
-            while(region_code_for_country_code(place)=="ZZ"):
-                place=random.randint(1,300)
+            while (region_code_for_country_code(place) == "ZZ"):
+                place = random.randint(1, 300)
 
         first = str(random.randint(100, 999))
         second = str(random.randint(1, 888)).zfill(3)
         last = str(random.randint(1, 9998)).zfill(4)
         return '+{}-{}-{}-{}'.format(str(place), first, second, last)
 
-    def gen_teltime(self,pick_call_distribution,delta_distribution):
+    def gen_teltime(self, pick_call_distribution, delta_distribution):
         """
         A dummy way to do this, to be continued.
         """
-        r=random.randint(0,6)
-        i=1
-        if(r==0):
-            call_distribution="midnight mode"
-        elif (r==1):
-            call_distribution="morning mode"
-        elif (r==2):
-            call_distribution="afternoon mode"
-        elif (r==3):
-            call_distribution="evening mode"
+        r = random.randint(0, 6)
+        i = 1
+        if (r == 0):
+            call_distribution = "midnight mode"
+        elif (r == 1):
+            call_distribution = "morning mode"
+        elif (r == 2):
+            call_distribution = "afternoon mode"
+        elif (r == 3):
+            call_distribution = "evening mode"
         else:
-            call_distribution=pick_call_distribution
+            call_distribution = pick_call_distribution
 
-        time=f1.create()
-        if(call_distribution=="midnight mode"):
+        # mktime(tuple)讲时间元组转换为本地时间
+        # 日期元组说明：年，月，日，时，分，秒，周，儒历日，夏令时
+        date1 = (2018, 1, 1, 0, 0, 0, -1, -1, -1)
+        time1 = mktime(date1)
+        date2 = (2020, 1, 1, 0, 0, 0, -1, -1, -1)
+        time2 = mktime(date2)
+        # 在这一范围内生成随机数
+        # starttime = uniform(time1, time2)
+        # localtime(seconds)将秒数转换为日期元组
+        # 打印例子：Wed Feb 24 05:29:22 2016
+
+        if (call_distribution == "midnight mode"):
             # TODO
-            starttime = time.iso8601(tzinfo=None)
-            while(starttime.split("T")[1].split(":")[0]>"6" and i<4):
-                starttime = time.iso8601(tzinfo=None)
-                i+=1
-        elif(call_distribution=="morning mode"):
+            # starttime = uniform(time1, time2)
+            starttime = asctime(localtime(uniform(time1, time2)))
+            while (starttime.split(" ")[3].split(":")[0] > "6" and i < 4):
+                starttime = asctime(localtime(uniform(time1, time2)))
+                i += 1
+        elif (call_distribution == "morning mode"):
             # TODO
-            starttime = time.iso8601(tzinfo=None)
-            while(starttime.split("T")[1].split(":")[0]>"12" or starttime.split("T")[1].split(":")[0]<"6" and i<4):
-                starttime = time.iso8601(tzinfo=None)
-                i+=1
-        elif(call_distribution=="afternoon mode"):
-            starttime = time.iso8601(tzinfo=None)
-            while(starttime.split("T")[1].split(":")[0]>"18" or starttime.split("T")[1].split(":")[0]<"12" and i<4):
-                starttime = time.iso8601(tzinfo=None)
-                i+=1
-        elif(call_distribution=="evening mode"):
-            starttime = time.iso8601(tzinfo=None)
-            while(starttime.split("T")[1].split(":")[0]<"18" and i<4):
-                starttime = time.iso8601(tzinfo=None)
-                i+=1
+            starttime = asctime(localtime(uniform(time1, time2)))
+            while (starttime.split(" ")[3].split(":")[0] > "12" or
+                   starttime.split(" ")[3].split(":")[0] < "6" and i < 4):
+                starttime = asctime(localtime(uniform(time1, time2)))
+                i += 1
+        elif (call_distribution == "afternoon mode"):
+            starttime = asctime(localtime(uniform(time1, time2)))
+            while (starttime.split(" ")[3].split(":")[0] > "18" or
+                   starttime.split(" ")[3].split(":")[0] < "12" and i < 4):
+                starttime = asctime(localtime(uniform(time1, time2)))
+                i += 1
+        elif (call_distribution == "evening mode"):
+            starttime = asctime(localtime(uniform(time1, time2)))
+            while (starttime.split(" ")[3].split(":")[0] < "18" and i < 4):
+                starttime = asctime(localtime(uniform(time1, time2)))
+                i += 1
         else:
-            starttime = time.iso8601(tzinfo=None)
+            starttime = asctime(localtime(uniform(time1, time2)))
 
         # If you are confused of distribution command, please refer to this tutorial:
         # https://blog.csdn.net/howhigh/article/details/78007317
-        if(delta_distribution=="exponential"):
+        if (delta_distribution == "exponential"):
             # TODO
             timedelta = np.random.exponential(600, 1)[0]
-        elif(delta_distribution=="poisson"):
+        elif (delta_distribution == "poisson"):
             # TODO
             timedelta = np.random.poisson(600, 1)[0]
-        elif(delta_distribution=="binomial"):
+        elif (delta_distribution == "binomial"):
             # TODO
-            timedelta = np.random.binomial(1200,0.5,1)[0]
+            timedelta = np.random.binomial(1200, 0.5, 1)[0]
         else:
             timedelta = random.randint(0, 6000)
             # timedelta = np.random.exponential(600, 1)[0]
             # timedelta = np.random.poisson(600, 1)[0]
             # timedelta = np.random.binomial(1200, 0.5, 1)[0]
 
-        return starttime+"|"+str(timedelta)
+        # asctime([tuple]) 将时间元组转换为字符串
+        return starttime + "|" + str(timedelta)
 
     def gen_teltype(self):
-        if(random.randint(0,1)==0):
+        if (random.randint(0, 1) == 0):
             return "SMS"
         else:
             return "VOICE"
@@ -137,67 +158,94 @@ class data_generator:
         return random.random()
 
     def gen_result(self):
-        r=random.randint(1,10)
-        if(r<9):
+        r = random.randint(1, 10)
+        if (r < 9):
             return "AWNSERED"
         else:
             return "Busy"
 
     def gen_type(self, pick_type_distribution, rate_type_distribution):
-        type={
+        type = {
+            # "Business",
             0: "Business",
-            1: "Agency",
-            2: "Education",
-            3: "Scam",
-            4: "Health",
-            5: "AD"
+            1: "Banking",
+            2: "Financial agency",
+            3: "Job",
+
+            #  "Agency",
+            4: "Legal agency",
+            5: "Housekeeping and property management",
+
+            #  "Education",
+            6: "School",
+            7: "Extracurricular training camp",
+
+            # "Health",
+            8: "Hospital (including health care)",
+            9: "Clinic (including dentist)",
+
+            # "AD",
+            10: "Food (including takeaway)",
+            11: "Dress code (booking and buying)",
+            12: "Housing (including rental)",
+            13: "Traveling",
+
+            14: "Emergency",
+            # "Private",
+            15: "Private",
+            16: "Private",
+            17: "Private",
+            18: "Private",
+            19: "Private"
         }
-        r = random.randint(0, 5)
-        if(pick_type_distribution=="default"):
+        r = random.randint(0, 20)
+        if (pick_type_distribution == "default"):
             return type.get(r)
 
-        r2=random.randint(1,100)
-        if(r2<100*rate_type_distribution):
+        r2 = random.randint(1, 100)
+        if (r2 < 100 * rate_type_distribution):
             return pick_type_distribution
         # r=(6N-1)/5
         return type.get(r)
 
     def output_redis_2(self):
-        rds_type.set(str(self.callednumber),str(self.type))
+        rds_type.set(str(self.callednumber), str(self.type))
 
 
 class people:
     """
-    A unique people with 1-5 telephones makes several calls
+    A unique people telephones makes several calls
     """
-    def __init__(self,ID=None,callnumber=None,calltimes=None,
-                 callednumber=None, teltime=None, teltype=None, charge=None, result=None, type=None,
+
+    def __init__(self, ID=None, callnumber=None, calltimes=None,
+                 callednumber=None, teltime=None, teltype=None, charge=None,
+                 result=None, type=None,
                  pick_type_distribution="default", rate_type_distribution=0.3,
                  pick_call_distribution="default", delta_distribution="default",
                  rate_place_distribution=0.7
                  ):
         # People
-        self.ID=self.gen_ID()
-        self.callnumber=self.gen_callnumber()
-        self.calltimes=self.gen_calltimes()
-        if ID is not None: self.ID=ID
+        self.ID = self.gen_ID()
+        self.callnumber = self.gen_callnumber()
+        self.calltimes = self.gen_calltimes()
+        if ID is not None: self.ID = ID
         if callnumber is not None: self.callnumber = callnumber
-        if calltimes is not None: self.calltimes=calltimes
+        if calltimes is not None: self.calltimes = calltimes
 
-        self.data=[]
-        self.gen_data(None, callednumber, teltime, teltype, charge, result, type,
+        self.data = []
+        self.gen_data(None, callednumber, teltime, teltype, charge, result,
+                      type,
                       pick_type_distribution, rate_type_distribution,
                       pick_call_distribution, delta_distribution,
                       rate_place_distribution)
 
-
     def __str__(self):
-        fuldata=""
+        fuldata = ""
         for i in range(self.calltimes):
-            tempdata=str(self.ID)+"|"+str(self.callnumber)+"|"\
-                     +(str(self.data[i]))
-            self.output_redis_1(self.data[i].ID,tempdata)
-            fuldata+=tempdata+"\n"
+            tempdata = str(self.ID) + "|" + str(self.callnumber) + "|" \
+                       + (str(self.data[i]))
+            self.output_redis_1(self.data[i].ID, tempdata)
+            fuldata += tempdata + "\n"
         return fuldata
 
     def gen_ID(self):
@@ -205,12 +253,12 @@ class people:
 
     def gen_callnumber(self):
         r = random.randint(1, 10)
-        if r<8:
-            place="1"
+        if r < 8:
+            place = "1"
         else:
             place = random.randint(1, 300)
-            while(region_code_for_country_code(place)=="ZZ"):
-                place=random.randint(1,300)
+            while (region_code_for_country_code(place) == "ZZ"):
+                place = random.randint(1, 300)
         first = str(random.randint(100, 999))
         second = str(random.randint(1, 888)).zfill(3)
         last = str(random.randint(1, 9998)).zfill(4)
@@ -218,35 +266,40 @@ class people:
         return '+{}-{}-{}-{}'.format(str(place), first, second, last)
 
     def gen_calltimes(self):
-        return random.randint(1,5)
+        return random.randint(10, 50)
 
-    def gen_data(self,ID= None, callednumber=None, teltime=None, teltype=None, charge=None, result=None, type=None,
+    def gen_data(self, ID=None, callednumber=None, teltime=None, teltype=None,
+                 charge=None, result=None, type=None,
                  pick_type_distribution="default", rate_type_distribution=0.3,
                  pick_call_distribution="default", delta_distribution="default",
                  rate_place_distribution=0.7):
         for i in range(self.calltimes):
-            data_generator_temp=data_generator(ID, callednumber, teltime, teltype, charge, result, type,
-                                               pick_type_distribution, rate_type_distribution,
-                                               pick_call_distribution, delta_distribution,
-                                               rate_place_distribution)
+            data_generator_temp = data_generator(ID, callednumber, teltime,
+                                                 teltype, charge, result, type,
+                                                 pick_type_distribution,
+                                                 rate_type_distribution,
+                                                 pick_call_distribution,
+                                                 delta_distribution,
+                                                 rate_place_distribution)
             self.data.append(data_generator_temp)
 
-    def output_redis_1(self,ID, tempdata):
-        rds.set(str(ID),str(tempdata))
+    def output_redis_1(self, ID, tempdata):
+        rds.set(str(ID), str(tempdata))
 
 
 # test generate
-# print("----------- test generate -----------")
-# d1 = data_generator(ID=1)
-# d2 = data_generator(pick_call_distribution="morning mode",pick_type_distribution="Education",rate_type_distribution=10)
-# print(d1)
-# print(d2)
-# print(d2.type)
+print("----------- test generate -----------")
+d1 = data_generator(ID=1)
+d2 = data_generator(pick_call_distribution="morning mode",
+                    pick_type_distribution="default", rate_type_distribution=10)
+print(d1)
+print(d2)
+print(d2.type)
 
-# # test people
-# print("------------ test people ------------")
-# p1 = people()
-# print(p1)
+# test people
+print("------------ test people ------------")
+p1 = people()
+print(p1)
 
 # # test iterate redis datas
 # print("-------------- test iterate redis data --------------")
@@ -257,4 +310,3 @@ class people:
 # print("-------------- test iterate redis_type data --------------")
 # keys_type = rds_type.keys()
 # print(keys_type)
-
---
+++ cdr_controller/template_05.py
@@ -0,0 +1,103 @@
+__author__ = "Wenjie Chen"
+__email__ = "wc2685@columbia.edu"
+
+import redis
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+
+class template_3:
+    """
+    The third template to analyze the country of callednumber
+    """
+    default_connect_info = {
+        'host': 'localhost',
+        'user': 'root',
+        'db': '0',
+        'port': 6379
+    }
+
+    def __init__(self, connect_info=None):
+
+        self.connect_info = connect_info
+        if connect_info == None:
+            self.connect_info = self.default_connect_info
+
+        rds = redis.Redis(host=self.connect_info['host'],
+                          port=self.connect_info['port'],
+                          decode_responses=True,
+                          db=self.connect_info['db']
+                          )  # host是redis主机，需要redis服务端和客户端都启动 redis默认端口是6379
+
+        pipe = rds.pipeline()
+        pipe_size = 100000
+        len = 0
+        key_list = []
+        value_list = []
+        # print(r.pipeline())
+        keys = rds.keys()
+        for key in keys:
+            key_list.append(key)
+            pipe.get(key)
+            if len < pipe_size:
+                len += 1
+            else:
+                for (k, v) in zip(key_list, pipe.execute()):
+                    print(k, v)
+                len = 0
+                key_list = []
+
+        for (k, v) in zip(key_list, pipe.execute()):
+            value_list.append(str(v))  # value_list
+
+        spark = SparkSession.builder.appName('myApp').getOrCreate()
+        sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
+
+        self.lines = sc.parallelize(value_list)
+        # print('show value list',value_list)
+
+    def __str__(self):
+        pass
+
+    def count_calltime(self, n):
+        """
+        This function is to read n lines data from redis, then count the call time duration sum of every hour.
+        """
+        # Drop all invalid data. TODO
+        people_calltime = self.lines.map(
+            lambda x: (x.split("|")[0], x.split("|")[4]))
+
+        people_calltime_w = people_calltime.map(
+            lambda x: (x[0] + ":" + x[1].split(" ")[0], 1))
+        people_calltime_d = people_calltime.map(
+            lambda x: (x[0] + ":" + x[1].split(" ")[3].split(":")[0], 1))
+
+        people_calltime_w_count = people_calltime_w.reduceByKey(
+            lambda x, y: x + y).map(
+            lambda x: (x[0].split(":")[0], x[0].split(":")[1], x[1]))
+        people_calltime_d_count = people_calltime_d.reduceByKey(
+            lambda x, y: x + y).map(
+            lambda x: (x[0].split(":")[0], x[0].split(":")[1], x[1]))
+        # print(people_calltime.take(20))
+        # print(people_calltime_w_count.take(20))
+        # print(people_calltime_d_count.take(20))
+
+        people_w = people_calltime_w_count.sortBy(
+            lambda x: (x[0], -x[2], x[1])).map(
+            lambda x: (x[0], x[1])).distinct().reduceByKey(lambda x, y: x)
+        people_d = people_calltime_d_count.sortBy(
+            lambda x: (x[0], -x[2], x[1])).map(
+            lambda x: (x[0], x[1])).distinct().reduceByKey(lambda x, y: x)
+
+        print(people_w.take(20))
+        print(people_d.take(20))
+
+        # ptest=people_calltime.map(lambda x:(x[0],1)).reduceByKey(lambda x,y:x+y)
+        # print(ptest.take(20))
+        # ptest2=people_calltime_w_count.sortBy(lambda x: (x[0],-x[2],x[1]))
+        # print(ptest2.take(50))
+
+
+test_temp_3 = template_3()
+test_temp_3.count_calltime(None)
