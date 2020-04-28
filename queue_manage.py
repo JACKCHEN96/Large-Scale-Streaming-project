b744fc56142eae0441253a301495b3dc570f636f
victorliu1994 <victorliu1994@126.com>
2020-04-28 11:10:58 -0400

    queue manage update

--- queue_manage.py
+++ queue_manage.py
@@ -7,49 +7,40 @@ import threading
 rds = redis.Redis(host='localhost', port=6379, decode_responses=True, db=0)
 data = ""
 flag = 0
+addr_list = []
 
 def test():
     global flag, data
-    if rds.llen('ID') > 100 and flag == 0:
+    l = rds.llen('ID')
+    if l > 50 and flag == 0:
         data = ""
-        # l = rds.llen('ID')
-        for i in range(100):
+        for i in range(50):
             t = rds.rpop('ID')
             data += t
             data += '\n'
         flag = 1
+        print('test')
     else:
         time.sleep(1)
-        print('flag')
 
 if __name__ == '__main__':
     s0 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s0.bind(('localhost', 9000))
-    s1.bind(('localhost', 9001))
-    s2.bind(('localhost', 9002))
-    print('initial')
     try:
-        print('initial 0')
-        s0.listen(1)
-        s1.listen(1)
-        s2.listen(1)
-        print('initial 1')
+        s0.listen(5)
         while True:
             test()
+            print(flag)
+            print(data)
             if flag != 0 and data != "":
-                conn, addr = s0.accept()
-                conn.sendall(data)
-                conn.close()
-                conn, addr = s1.accept()
-                conn.sendall(data)
-                conn.close()
-                conn, addr = s2.accept()
-                conn.sendall(data)
-                conn.close()
-                flag = 0
-                print('send')
+                try:
+                    conn, addr = s0.accept()
+                except OSError:
+                    print("Nothing")
+                else:
+                    print(addr)
+                    conn.sendall(bytes(data, 'utf-8'))
+                    print('send')
             else:
                 print('wait')
     except KeyboardInterrupt:
