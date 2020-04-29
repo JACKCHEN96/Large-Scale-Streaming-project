d59576c1f3e01db7e9d4b6c034872515d17e7d89
JACKCHEN96 <cchenwenjie0901@gmail.com>
2020-04-29 00:02:11 -0400

    update tmp5 and res

--- cdr_controller/filters/template_05.py
+++ cdr_controller/filters/template_05.py
@@ -8,8 +8,8 @@ from pyspark.sql.functions import *
 from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
 
-rds_temp = redis.Redis(host='localhost', port=6379, decode_responses=True,
-                       db=6)  # host是redis主机，需要redis服务端和客户端都启动 redis默认端口是6379
+# rds_temp = redis.Redis(host='localhost', port=6379, decode_responses=True,
+#                        db=6)  # host是redis主机，需要redis服务端和客户端都启动 redis默认端口是6379
 
 # create spark context
 spark = SparkSession.builder.appName('myApp').getOrCreate()
@@ -57,36 +57,19 @@ class template_5:
             lambda x, y: x + y).map(
             lambda x: (x[0].split(":")[0], x[0].split(":")[1], x[1]))
 
-        # print(people_calltime.take(20))
-        # print(people_calltime_w_count.take(20))
-        # print(people_calltime_d_count.take(20))
-
-        def save_rdd_1(rdd):
-            if not rdd.isEmpty():
-                df1 = rdd.sortBy(lambda x: (x[0], -x[2], x[1])) \
-                    .map(lambda x: (x[0], x[1])).distinct().reduceByKey(lambda x, y: x) \
-                    .toDF()
-                # df.show()
-                df1.write.format("org.apache.spark.sql.redis").option("table", "counts_5_1").option("key.column",
-                                                                                                    "_1").save(
-                    mode='overwrite')
-
-        def save_rdd_2(rdd):
-            if not rdd.isEmpty():
-                df2 = rdd.sortBy(lambda x: (x[0], -x[2], x[1])) \
-                    .map(lambda x: (x[0], x[1])).distinct().reduceByKey(lambda x, y: x) \
-                    .toDF()
-                # df.show()
-                df2.write.format("org.apache.spark.sql.redis").option("table", "counts_5_2").option("key.column",
-                                                                                                    "_1").save(
-                    mode='overwrite')
-
-        # people_calltime_w_count.foreachRDD(save_rdd_1)
-        # people_calltime_d_count.foreachRDD(save_rdd_2)
-        people_calltime_w_count.foreachRDD(lambda rdd: print(rdd.sortBy(lambda x: (x[0], -x[2], x[1])).map(lambda x: (x[0], x[1])).distinct().reduceByKey(lambda x, y: x).collect()))
-        people_calltime_d_count.foreachRDD(lambda rdd: print(rdd.sortBy(lambda x: (x[0], -x[2], x[1])).map(lambda x: (x[0], x[1])).distinct().reduceByKey(lambda x, y: x).collect()))
         # people_calltime_w_count.pprint()
         # people_calltime_d_count.pprint()
+
+        # people_calltime_count_total=people_calltime_w_count.union(people_calltime_d_count)
+        # people_calltime_count_total.pprint()
+
+        people_calltime_w_count.foreachRDD(lambda rdd: rdd.sortBy(lambda x: (x[0], -x[2], x[1])).map(lambda x: (x[0], x[1])).distinct().reduceByKey(lambda x, y: x)
+                                            .sortBy(lambda x: x[0]).toDF().toPandas().to_json("../../res/tmp5/day.json"))
+
+        people_calltime_d_count.foreachRDD(lambda rdd: rdd.sortBy(lambda x: (x[0], -x[2], x[1])).map(lambda x: (x[0], x[1])).distinct().reduceByKey(lambda x, y: x)
+                                            .sortBy(lambda x: x[0]).toDF().toPandas().to_json("../../res/tmp5/clock.json"))
+
+
 
 test_temp_5 = template_5(IP="localhost",port=9000)
 test_temp_5.count_calltime(None)
