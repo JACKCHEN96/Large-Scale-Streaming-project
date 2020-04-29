cafcafaed31c59a4ad1a70fabc1239a971703f14
victorliu1994 <victorliu1994@126.com>
2020-04-29 09:08:10 -0400

    queue manage update, two parameters, port and listName

--- queue_manage.py
+++ queue_manage.py
@@ -1,20 +1,19 @@
-import json
+import sys
 import socket
 import redis
 import time
-import threading
 
 rds = redis.Redis(host='localhost', port=6379, decode_responses=True, db=0)
 data = ""
 flag = 0
 
-def test():
+def test(Name):
     global flag, data
-    l = rds.llen('ID')
+    l = rds.llen(Name)
     if l > 50 and flag == 0:
         data = ""
         for i in range(50):
-            t = rds.rpop('ID')
+            t = rds.rpop(Name)
             data += t
             data += '\n'
         flag = 1
@@ -23,12 +22,15 @@ def test():
         time.sleep(1)
 
 if __name__ == '__main__':
+    args = sys.argv[1:]
+    port = int(args[0])
+    listName = args[1]
     s0 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s0.bind(('localhost', 9000))
+    s0.bind(('localhost', port))
     s0.listen(5)
     conn, addr = s0.accept()
     while True:
-        test()
+        test(Name = listName)
         print(flag)
         if flag != 0 and data != "":
             conn.sendall(bytes(data, 'utf-8'))
@@ -36,5 +38,3 @@ if __name__ == '__main__':
             print('send')
         else:
             print('wait')
-
-
