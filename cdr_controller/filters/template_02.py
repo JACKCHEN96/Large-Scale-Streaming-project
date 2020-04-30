d3949cdb5a5b40a4f1be2f9d5d77efcf0841a26e
JACKCHEN96 <cchenwenjie0901@gmail.com>
2020-04-29 20:15:44 -0400

    update tmp2

--- cdr_controller/filters/template_02.py
+++ cdr_controller/filters/template_02.py
@@ -15,10 +15,10 @@ STORE_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path
 
 class template_02:
     """
-    The third template to analyze the country of callednumber
+    The third template to analyze the type/tag of people
     """
 
-    def __init__(self, IP="localhost", interval=10, port=9003):
+    def __init__(self, IP="localhost", interval=10, port=9002):
         self.spark = SparkSession.builder.appName('template2').getOrCreate()
         self.sc = SparkContext.getOrCreate(SparkConf().setMaster("local"))
 
@@ -40,7 +40,8 @@ class template_02:
 
     def count_tag(self, n):
         """
-        This function is to read n lines data from redis, then count the call time duration sum of every hour.
+        This function is to analyze people tag.
+        For example, Bob call: Housekeeping and property management *10, Banking *8, Financial agency *5. Then he is a business tag man(13), hpm type man(10).
         """
 
         # Drop all invalid data. TODO
@@ -49,20 +50,52 @@ class template_02:
             rds_type = redis.Redis(host="localhost", port=6379, decode_responses=True,
                                    db=1)  # host是redis主机，需要redis服务端和客户端都启动 redis默认端口是6379
 
-            return (x.split("|")[0],"private") if rds_type.get(x.split("|")[3])==None else (x.split("|")[0],rds_type.get(x.split("|")[3]))
+            return (x.split("|")[0],"Private") if rds_type.get(x.split("|")[3])==None else (x.split("|")[0],rds_type.get(x.split("|")[3]))
+
+        def mapper(x):
+
+            tag = {
+                # "Business",
+                "Business": "Business",
+                "Banking": "Business",
+                "Financial agency": "Business",
+                "Job": "Business",
+                #  "Agency",
+                "Legal agency": "Agency",
+                "Housekeeping and property management": "Agency",
+                #  "Education",
+                "School": "Education",
+                "Extracurricular training camp": "Education",
+                # "Health",
+                "Hospital (including health care)": "Health",
+                "Clinic (including dentist)": "Health",
+                # "AD",
+                "Food (including takeaway)": "AD",
+                "Dress code (booking and buying)": "AD",
+                "Housing (including rental)": "AD",
+                "Traveling": "AD",
+                "Emergency": "Emergency",
+                "Private": "Private"
+            }
+            return (x[0],"Private",x[2]) if tag.get(x[1])==None else (x[0], tag.get(x[1]), x[2])
+
 
         process_lines=self.lines.map(helper)
-        process_lines.pprint()
-        process_lines.countByValue().pprint()
-        process_lines.countByValue().reduceByKey(lambda x,y: x+y).pprint()
+        # process_lines.pprint()
+        people_type_count=process_lines.countByValue().map(lambda x: (x[0][0],x[0][1],x[1]))
+        people_type_count.pprint()
+        # First, people with type
+        people_type_max=people_type_count.transform(lambda rdd: rdd.sortBy(lambda x: (x[0],-int(x[2]),x[1])).map(lambda x: (x[0],x[1])).reduceByKey(lambda x,y:x))
 
-        # resultstream = (process_lines
-        #                 .map(lambda word: (word, 1))  # 将word映射成(word,1)
-        #                 .reduceByKey(lambda x, y: x + y))  # reduceByKey对所有有着相同key的items执行reduce操作
-        #
-        # resultstream.pprint()
-        #
-        # resultstream.foreachRDD(lambda rdd: rdd.sortBy(lambda x: x[0]).toDF().toPandas().to_json(os.path.join(STORE_DIR, "tmp2", "tag.json")) if not rdd.isEmpty() else None)
+        people_type_max.pprint()
+        people_type_max.foreachRDD(lambda rdd: rdd.sortBy(lambda x: x[0]).toDF().toPandas().to_json(os.path.join(STORE_DIR, "tmp2", "pptype.json")) if not rdd.isEmpty() else None)
+        # Second, people with tag
+        people_tag=people_type_count.map(mapper)
+        people_tag_max=people_tag.transform(lambda rdd: rdd.sortBy(lambda x: (x[0],-int(x[2]),x[1])).map(lambda x: (x[0],x[1])).reduceByKey(lambda x,y:x))
+
+        people_type_max.foreachRDD(lambda rdd: rdd.sortBy(lambda x: x[0]).toDF().toPandas().to_json(os.path.join(STORE_DIR, "tmp2", "pptag.json")) if not rdd.isEmpty() else None)
+        people_tag_max.pprint()
+
 
 
 def template_2_main():
